e146ab3caa073c9e45289c82b516777deaf5ef96
Jantastication O <georginaoregx@gmail.com>
2018-09-14 23:51:50 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,24 +1,18 @@
 board = [" ", " " ," " ," " ," ", " ", " ", " ", " "]
 def display_board(board)
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
+ end
+ 
+board = [" ", " " ," " ," " ," ", " ", " ", " ", " "]
+def display_board(board)
    puts "  #{board[0]} | #{board[1]}  | #{board[2]}  "
    puts "-----------"
    puts "  #{board[3]} | #{board[4]}  | #{board[5]}  "
    puts "-----------"
    puts "  #{board[6]} | #{board[7]}  |  #{board[8]} "
 end
-board = [" ", " " ," " ," " ," ", " ", " ", " ", " "]
-def display_board(board)
-   puts "   |   |   "
-   puts "-----------"
-   puts "   |   |   "
-   puts "-----------"
-   puts "   |   |   "
-end
-board [4] = x
-def display_board(board)
-  puts "  #{board[0]} | #{board[1]}  | #{board[2]}  "
-  puts "-----------"
-  puts "  #{board[3]} | #{board[4]}  | #{board[5]}  "
-  puts "-----------"
-  puts "  #{board[6]} | #{board[7]}  |  #{board[8]} "
-end
+
