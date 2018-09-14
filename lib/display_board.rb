cca3b18ba07e237513bb4ed81656756d6e2693ec
Jantastication O <georginaoregx@gmail.com>
2018-09-14 23:57:24 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -16,12 +16,11 @@ def display_board(board)
    puts "  #{board[6]} | #{board[7]}  |  #{board[8]} "
 end
 
-def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+def display_board(board = [" ", " ", " ", " ", "x", " ", " ", " ", " "])
   puts 'display board'
-#board output
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
