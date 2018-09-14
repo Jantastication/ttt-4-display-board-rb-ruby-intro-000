6abd749219e3f59169f4fac62072dac55b9e76a3
Jantastication O <georginaoregx@gmail.com>
2018-09-14 23:21:28 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,3 +7,11 @@ def display_board(board)
    puts "-----------"
    puts "  #{board[6]} | #{board[7]}  |  #{board[8]} "
 end
+board = [" ", " " ," " ," " ," ", " ", " ", " ", " "]
+def display_board(board)
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
+end
