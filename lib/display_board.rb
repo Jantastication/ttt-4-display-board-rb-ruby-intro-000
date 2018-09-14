323ba655500ab11784f2a03512baf4c211ddc5a4
Jantastication O <georginaoregx@gmail.com>
2018-09-14 23:55:28 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-board = [" ", " " ," " ," " ," ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
    puts "   |   |   "
    puts "-----------"
@@ -7,11 +7,21 @@ def display_board(board)
    puts "   |   |   "
  end
 
-board = [" ", " " ," " ," " ," ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
    puts "  #{board[0]} | #{board[1]}  | #{board[2]}  "
    puts "-----------"
    puts "  #{board[3]} | #{board[4]}  | #{board[5]}  "
    puts "-----------"
    puts "  #{board[6]} | #{board[7]}  |  #{board[8]} "
+end
+
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts 'display board'
+#board output
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
